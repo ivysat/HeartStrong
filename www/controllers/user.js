a672a721e10a7db56f32708e60f5608b61126e93
SonJoseph <joeson34@cs.unc.edu>
2018-03-19 14:05:56 -0400

    login form

--- www/controllers/user.js
+++ www/controllers/user.js
@@ -1,14 +1,28 @@
-var app = angular.module('myApp', []);
+var app = angular.module('userApp', []);
 
-app.controller('registerCtrl', function($scope, $http){
+app.controller('userCtrl', function($scope, $http){
+    
+    $scope.show = function(element_id){
+        $(element_id).show();
+        switch(element_id) {
+            case "#registerForm":
+                $("#loginForm").hide();
+                break;
+            case "#loginForm":
+                $("#registerForm").hide();
+                break;
+        }
+    }
     $http.get("http://sonjoseph.website/heartstrong_backend/connect.php").then(function(res){
         
     });
+    
     var config = {
         headers : {
             'Content-Type': 'application/x-www-form-urlencoded;charset=utf-8;'
         }
     }
+    
     $scope.register = function(){
         // use $.param jQuery function to serialize data from JSON
         var data = $.param({
@@ -21,4 +35,10 @@ app.controller('registerCtrl', function($scope, $http){
             
         });
     }
+    
+    $scope.login = function(){
+        console.log($scope.username);
+        console.log($scope.password);
+    }
+    
 });
